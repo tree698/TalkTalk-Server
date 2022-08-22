c14e831e8a505f0bb6afd88c33f0690b05e7deb0
Chanwoo <tree698@gmail.com>
2022-08-22 13:12:48 +0900

    connect mysql db with sequelize

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('finally start...');
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+import 'express-async-errors';
+import { config } from './config.js';
+import { sequelize } from './db/database.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.sendStatus(500);
+});
+
+// ToDo: socket.io 연결
+sequelize.sync().then(() => {
+  app.listen(config.host.port);
+});
