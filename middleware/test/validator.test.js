882ad1f6bf51f30f8ad315db6292b7aff6ed18c0
Chanwoo <tree698@gmail.com>
2023-01-07 23:38:12 +0900

    test code: validator.js

---
+++ middleware/test/validator.test.js
@@ -0,0 +1,39 @@
+import httpMocks from 'node-mocks-http';
+import { validate } from '../validator.js';
+import * as validator from 'express-validator';
+import { faker } from '@faker-js/faker';
+
+jest.mock('express-validator');
+
+describe('Validator Middleware', () => {
+  let req;
+  let res;
+  let next;
+  beforeEach(() => {
+    req = httpMocks.createMocks();
+    res = httpMocks.createResponse();
+    next = jest.fn();
+  });
+
+  it('calls next if there are no validation errors', async () => {
+    validator.validationResult = jest.fn((req) => ({ isEmpty: () => true }));
+
+    await validate(req, res, next);
+
+    expect(next).toBeCalled();
+  });
+
+  it('returns 400 if there are validation errors', async () => {
+    const errorMsg = faker.word.noun(1);
+    validator.validationResult = jest.fn((req) => ({
+      isEmpty: () => false,
+      array: () => [{ msg: `${errorMsg}` }],
+    }));
+
+    await validate(req, res, next);
+
+    expect(next).not.toBeCalled();
+    expect(res.statusCode).toBe(400);
+    expect(res._getJSONData().message).toBe(`${errorMsg}`);
+  });
+});
